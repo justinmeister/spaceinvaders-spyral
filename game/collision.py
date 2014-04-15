e735751db21a7595741fb8a62a875a08c95955af
Justin Armstrong <justinmeister@gmail.com>
2014-04-14 17:33:00 -0700

    updated collision methods

--- game/collision.py
+++ game/collision.py
@@ -11,13 +11,16 @@ class CollisionHandler(object):
         self.player = None
         self.alien_list = []
         self.bullet_list = []
-        self.sprites_to_kill = []
+        self.aliens_to_kill = []
+        self.bullets_to_kill = []
+        self.refresh_count = 0
         spyral.event.register('director.update', self.update)
 
     def update(self):
         """
         Check if there is a collision.
         """
+        self.refresh_count += 1
         self.check_for_bullet_and_alien_collision()
         self.check_if_bullet_off_screen()
         self.kill_dead_sprites()
@@ -32,27 +35,29 @@ class CollisionHandler(object):
             for j, bullet in enumerate(self.bullet_list):
                 if bullet.collide_sprite(alien):
                     self.make_explosion(alien.x, alien.y)
-                    self.sprites_to_kill.append(alien)
-                    self.sprites_to_kill.append(bullet)
-                    self.alien_list.pop(i)
-                    self.bullet_list.pop(j)
-
+                    self.aliens_to_kill.append((i, alien))
+                    self.bullets_to_kill.append((j, bullet))
 
     def check_if_bullet_off_screen(self):
         for i, bullet in enumerate(self.bullet_list):
             if bullet.y < 0:
-                self.sprites_to_kill.append(bullet)
+                self.bullets_to_kill.append((i, bullet))
                 self.bullet_list.pop(i)
 
-
     def kill_dead_sprites(self):
-        for sprite in self.sprites_to_kill:
+        for index, sprite in self.aliens_to_kill:
+            self.alien_list.pop(index)
             sprite.kill()
+            
+        for index, sprite in self.bullets_to_kill:
+            self.bullet_list.pop(index)
+            sprite.kill()
+
+        self.aliens_to_kill = []
+        self.bullets_to_kill = []
 
     def make_explosion(self, x, y):
         explosion = sprite.Explosion(x, y, self.scene)
-
-
 
 
     def add_bullet(self, bullet):
